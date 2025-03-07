63c548b270f7fd285387686636d063285dfae2b3
Akshay Ladne <Kumawata283@gmail.com>
2025-03-07 19:28:30 +0530

    make few changes to implement logics

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,51 +1,59 @@
-import { useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [counterVisible, setCounterVisible] = useState(true)
 
-  let [counter, setCounter] = useState(5);
-  
-  // let counter = 5;
+  //for conditional rendoring
+  useEffect(() => {
 
-  const addValue = ()=>{
-    try {
-      if (counter >= 20) {
-         alert("20 is the limit."); 
-        return;
-      }
-      
-    } catch (error) {
-      console.log("Caught an error:");
+    const intervalId = setInterval(() => {
+      setCounterVisible(count => !count)
+    }, 5000);
+
+    return function() {
+      clearInterval(intervalId);
     }
-    setCounter(counter+1);
+  }, [])
 
-  }
-  const decreaseValue = ()=>{
-    try {
-      if (counter <= 0) {
-        throw alert("counter won't go in negative.");
-        // return; // throw stop the execution of function immediately 
-        // and return does the same thing that's why return is not reachable here
-      }
-      
-    } catch (error) {
-      console.log("Caught an error:");
-      return;
+  return (
+    <div>
+      hi
+      {counterVisible && <Counter></Counter>}
+      hello
+    </div>
+  )
+}
+
+// To increase the counter automatically
+function Counter() {
+  let [counter, setCounter] = useState(0);
+
+  useEffect(function () {
+    console.log("on mount");
+
+    const intervalId = setInterval(function () {
+      console.log("from inside interval");
+      setCounter(count => count + 1);
+
+    }, 1000)
+
+    return function() {
+      console.log("on unmount");
+      clearInterval(intervalId);
     }
-    setCounter(counter-1);
-  }
+  }, []);
+
+
   return (
     <>
       <h1>Hello there!👋</h1>
       <h2>Counter value: {counter}</h2>
-
-      <button onClick = {addValue} >Increase value {counter}</button>
-      <br />
-      <button onClick = {decreaseValue}>Decrease value {counter}</button>
     </>
   )
 }
+
 
 export default App
