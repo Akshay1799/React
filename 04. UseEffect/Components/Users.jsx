0bb1a86ced781334629dcb28189aca8ee6ed244c
Akshay Ladne <Kumawata283@gmail.com>
2025-06-02 23:37:24 +0530

    initial commit

---
+++ 04. UseEffect/Components/Users.jsx
@@ -0,0 +1,38 @@
+import React, { useState,useEffect } from 'react'
+
+const Users = () => {
+
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+
+    const apiData = async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users')
+      const data = await response.json()
+      setUser(data);
+      console.log(data);
+    }
+    apiData();
+  }, [])
+
+
+  return (
+    <>
+    
+      <div>
+        <ul style={{display:'Grid', gridTemplateColumns:'350px 300px 350px'}}>
+          {user.map((user)=>(
+            <li key={user.id} style={{boxShadow: 'rgba(0, 0, 0, 0.16) 0px 1px 4px',listStyle:'none', margin:5, padding:10, border:'0.5px solid grey', borderRadius:6}}>
+              <h4>{user.name}</h4>
+              <p>{user.username}</p>
+              <p>{user.email}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+    </>
+  )
+}
+
+export default Users
