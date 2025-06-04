3eb33fedef6b775341126bb2aff978bada10f268
Akshay Ladne <Kumawata283@gmail.com>
2025-06-04 14:08:01 +0530

    initial commit

---
+++ 06. Routing/src/Components/Routing.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import{BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from '../Pages/Home'
+import Services from '../Pages/Services'
+import Contact from '../Pages/Contact'
+import About from '../Pages/About'
+import Multi_Input_Form from '../../../05. Form/src/Components/Multi_Input_Form'
+import Users from '../../../04. UseEffect/Components/Users'
+
+
+const Routing = () => {
+  return (
+    <>
+    <Router>
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/Services' element={<Services/>}/>
+            <Route path='/Contact' element={<Contact/>}/>
+            <Route path='/About' element={<About/>}/>
+            <Route path='/users' element={<Users/>}/>
+            <Route path='/form' element={<Multi_Input_Form/>}/>
+        </Routes>
+    </Router>
+    </>
+  )
+}
+
+export default Routing
