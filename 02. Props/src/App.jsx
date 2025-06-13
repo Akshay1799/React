0ad0b30c334678b32ce6e8c170798e6f202b970b
Akshay Ladne <Kumawata283@gmail.com>
2025-06-13 12:54:08 +0530

    initial commit

---
+++ 02. Props/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="bg-blue-200 text-black p-5 font-bold">Hi, Akshay</h1>
+    </>
+  )
+}
+
+export default App
