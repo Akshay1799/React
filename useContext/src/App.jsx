7f0e1132c4fa065c3a8b0f396bd92d7ecdec4742
Akshay Ladne <Kumawata283@gmail.com>
2025-06-28 15:04:38 +0530

    initial commit

--- useContext/src/App.jsx
+++ useContext/src/App.jsx
@@ -1,16 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
-import userContext from './context/userContext'
+// import userContext from './context/userContext'
 import Navbar from './Components/Navbar'
+import themeContext from './context/themeContext'
+
 
 function App() {
-  const [user] = useState({name: 'Akshay'})
+  // const [user] = useState({name: 'Akshay'})
+  const [theme, setTheme] = useState('light')
 
+  const toggleTheme = () => setTheme(prev => prev === 'light' ? 'dark' : 'light')
+  
+  useEffect(() => {
+        document.body.style.backgroundColor = theme === 'light' ? '#ffffff' : '#121212',
+        document.body.style.color = theme === 'light' ? '#000000' : '#ffffff',
+        document.body.style.transition = '0.3s ease-in-out' // smooth transition
+        }, [theme]);
+  
   return (
     <>
-      <userContext.Provider value={user}>
+    // for user
+      {/* <userContext.Provider value={user}>
         <Navbar/>
-      </userContext.Provider>
+      </userContext.Provider>  */}
+
+    // for theme
+      <themeContext.Provider value={{ theme, toggleTheme }}>
+        <div style={{padding:'2rem'}}>
+          <Navbar />
+        </div>
+      </themeContext.Provider >
+
     </>
   )
 }
--- useContext/src/Components/Navbar.jsx
+++ useContext/src/Components/Navbar.jsx
@@ -1,12 +1,22 @@
 import React,{useContext} from "react"
-import userContext from "../context/userContext"
+// import userContext from "../context/userContext"
+import themeContext from "../context/themeContext";
+
 const Navbar = () => {
 
-    const user = useContext(userContext);
-    const  {name} = user;
-  return (
-    <>
-        <h1>{`Hello ${name}`}</h1>
+  // for user
+    // const user = useContext(userContext);
+    // const  {name} = user; // Object destructuring
+
+
+  // for theme
+    const {theme, toggleTheme} = useContext(themeContext);
+    // here we have destructure the object
+    return (
+      <>
+        {/* <h1>{`Hello ${name}`}</h1> */}
+        <h3>{`Current theme: ${theme}`}</h3>
+      <button onClick = {toggleTheme}>Toggle theme</button>
     </>
   )
 }
