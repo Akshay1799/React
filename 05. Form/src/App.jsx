60515383e92449ac34ba002a9faeb6a801de4f57
Akshay Ladne <Kumawata283@gmail.com>
2025-07-18 00:13:40 +0530

    initial commit

--- 05. Form/src/App.jsx
+++ 05. Form/src/App.jsx
@@ -1,17 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 import Form from './Components/Form'
 import Multi_Input_Form from './Components/Multi_Input_Form'
+
 
 function App() {
  
 
   return (
     <>
+
       {/* <Form/> */}
       <Multi_Input_Form/>
+      
     </>
   )
 }
